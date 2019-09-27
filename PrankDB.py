a51d4a37beb48328eec702cb67a16eb441ea83c9
WyoHutch <ghutchinson@digis.net>
2019-09-27 15:10:08 -0600

    Flask module installed

--- PrankDB.py
+++ PrankDB.py
@@ -1,33 +1,39 @@
+from flask import Flask, render_template, request, jsonify
 import pymongo
 import datetime
 
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
 prankclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
 prankdb = prankclient["prankdatabase"]
-# print(prankclient.list_database_names())
+# dblist = prankclient.list_database_names()
+prankscol =prankdb["pranks"]
 
-dblist = prankclient.list_database_names()
+# nextdict = [
+#     {"title": "Title One", "rules": "Rule One", "location": "Somwhere One", "person": "Someone One", "difficulty": 2},
+#     {"title": "Title Two", "rules": "Rule Two", "location": "Somwhere Two", "person": "Someone Two", "difficulty": 4},
+#     {"title": "Title Three", "rules": "Rule Three", "location": "Somwhere Three", "person": "Someone Three", "difficulty": 6},
+#     {"title": "Title Four", "rules": "Rule Four", "location": "Somwhere Four", "person": "Someone Four", "difficulty": 8},
+#     {"title": "Title Five", "rules": "Rule Five", "location": "Somwhere Five", "person": "Someone Five", "difficulty": 1},
+#     {"title": "Title Six", "rules": "Rule Six", "location": "Somwhere Six", "person": "Someone Six", "difficulty": 3},
+#     {"title": "Title Seven", "rules": "Rule Seven", "location": "Somwhere Seven", "person": "Someone Seven", "difficulty": 5}
+# ]
 
-prankcol = prankdb["pranks"]
+# x = prankscol.insert_many(nextdict)
 
-inidict = [
-    {"prank": "Prank One", "timeStamp": datetime.datetime.now(), "rank": 2, "tempField": "temp"},
-    {"prank": "Prank Two", "timeStamp": datetime.datetime.now(), "rank": 5, "tempField": "temp"},
-    {"prank": "Prank Three", "timeStamp": datetime.datetime.now(), "rank": 3, "tempField": "temp"},
-    {"prank": "Prank Four", "timeStamp": datetime.datetime.now(), "rank": 1, "tempField": "temp"},
-    {"prank": "Prank Five", "timeStamp": datetime.datetime.now(), "rank": 4, "tempField": "temp"},
-    {"prank": "Prank Six", "timeStamp": datetime.datetime.now(), "rank": 2, "tempField": "temp"},
-    {"prank": "Prank Seven", "timeStamp": datetime.datetime.now(), "rank": 4, "tempField": "temp"},
-]
 
-# @app.route("/")
+@app.route('/', methods = ['GET'])
+
 def data_ret():
     ret_list = [];
-    for x in prankcol.find():
+    for x in prankscol.find():
         x.pop('_id');                #Keeps or removes the _id item from the dictionary
-        # print(x)
         ret_list.append(x)
-    return ret_list
+    return {"Object": ret_list}
+    # return ret_list
 
-# print(ret_list)
 print(data_ret())
+
+if __name__ == "__main__":
+    app.run(debug=True)
